c61a4b95148d38f22729bda136dfdbdc85fd080d
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-03-03 22:52:21 +0300

    feat: implement simple adepteve

--- game/src/components/footer/Footer.tsx
+++ game/src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer: React.FC = () => {
     <footer className="page-footer brown">
       <div className="container-left">
         <div className="row">
-          <div><span>Created by - </span>
+          <div ><span>Created by - </span>
             <a className="grey-text text-lighten-3" href="https://github.com/artemosadchuck">
               <strong>artemosadchuck</strong>
             </a>
